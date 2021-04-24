2e1b81b6d63c13e8cff15afed1e134b465bf9b03
GabrielHenriqueAlbino <albinogabriel53@gmail.com>
2021-04-24 15:41:21 -0300

    Subindo para o git

---
+++ SRC/Index.js
@@ -0,0 +1,54 @@
+//importando o pacote express
+const express = require ('express');
+const { uuid } = require ('uuidv4');
+
+
+//preparar para usar o express;
+const app = express();
+app.use(express.json());
+
+const repositories = [];
+// 1 paramentro o nome da rota 
+// 2 parametro ação que vou fazer 'função'
+//java script arrow function
+app.get('/',(request,response)=>{
+ return response.json(repositories);
+});
+
+
+app.post('/',(request,response)=>{
+    const {name , email, cpf, peso, altura } = request.body;
+    const newStudent = { id: uuid(), name, email, cpf, peso, altura };
+    repositories.push(newStudent);
+    return response.json({newStudent});
+  });
+
+    app.put('/:id', (request, response) => {
+    const { id } = request.params;
+    const { name, email, cpf, peso, altura } = request.body;
+    
+    const studentResearch = repositories.findIndex ( studentIndex => studentIndex.id == id );
+    if (studentResearch < 0 ){
+       return response.status(404).json({"error":"Student not found"});
+    }
+     const newStudent = {id, name, email, cpf, peso, altura};
+     repositories[studentResearch] = newStudent;
+     return response.json(newStudent);
+    });
+    
+    app.delete('/:id', (request, response) => {
+      const { id } = request.params;
+      const studentResearch = repositories.findIndex(studentIndex => studentIndex.id == id );
+    if (studentResearch < 0 ){
+       return response.status(404).json({ "error": `Student ${id} not found` });
+    }
+      repositories.splice(studentResearch,1);
+      return response.json({"Message" : `Student ${id} removed`});
+    
+    });
+
+    
+    module.exports = app.listen(3333, () => {
+        console.log("Server running");
+    });
+
---
+++ SRC/tests/app.test.js
@@ -0,0 +1,69 @@
+const app = require('../index');
+const request = require('supertest');
+const { uuid, isUuid } = require ('uuidv4');
+
+
+describe("Teste 1 - Criar novo estudante", () => {
+    it("POST - teste metodo POST estudante", async () => {
+        //chamada da API no metodo POST
+        const response = await request(app)
+            .post("/")
+            .send({
+                "name": "Gabriel",
+                "email": "gabriel@gmail.com"
+            })
+            .expect(200);
+        expect(isUuid(response.body.newStudent.id)).toBe(true);
+        expect(response.body).toMatchObject({
+            "newStudent": {
+                "name": "Gabriel",
+                "email": "gabriel@gmail.com"
+            }
+        });
+    });
+});
+
+describe("Teste 2 - deletar um estudante que não existe", () => {
+    it("DELETE - estudante 123 que não existe", async () => {
+        await request(app).delete('/123')
+            .expect(404);
+    });
+});
+
+describe("Teste 3 - atualização de um estudante", () => {
+    //2 passos cadastrar um estudante e depois eu vou atualizar ele
+    it("POST e PUT - teste metodo atualizar estudante", async () => {
+        //chamada da API no metodo POST
+        const response = await request(app)
+            .post("/")
+            .send({
+                "name": "Gabriel",
+                "email": "gabriel@gmail.com"
+            })
+            .expect(200);
+        expect(isUuid(response.body.newStudent.id)).toBe(true);
+        expect(response.body).toMatchObject({
+            "newStudent": {
+                "name": "Gabriel",
+                "email": "gabriel@gmail.com"
+            }
+        });
+        //atualizar esse aluno
+        const responseUpd = await request(app)
+            .put(`/${response.body.newStudent.id}`)
+            .send({
+                "name": "Gabriel Henrique Albino",
+                "email": "g1@gmail.com"
+            })
+            .expect(200);
+        expect(responseUpd.body).toMatchObject({
+            "name": "Gabriel Henrique Albino",
+            "email": "g1@gmail.com"
+        });
+    });
+});
+
+afterAll(done => {
+    app.close();
+    done();
+});
